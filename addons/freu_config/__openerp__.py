50e0338845000277f1efaefc35374a492ad2104f
Joachim Grubelnik <j.grubelnik@openat.at>
2021-12-09 14:47:36 +0100

    [ADD] adding fsonline customer Addon inklusive CI and the full customer addon

--- addons/freu_config/__openerp__.py
+++ addons/freu_config/__openerp__.py
@@ -1,27 +1,31 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'freu_config',
-    'summary': '''FS-Online freu instance configuration''',
+    'name': 'cona_config',
+    'summary': '''FS-Online Cona instance configuration''',
     'description': '''
 FS-Online Instance Configuration
 ================================
 
-Customer configuration for the instance freu
+Customer configuration for the instance cona
 
 - Default settings
 - View modifications
 - CSS
 - Translations
     ''',
-    'author': 'Michael Karrer (michael.karrer@datadialog.net), DataDialog',
+    'author': 'Joachim Grubelnik (joachim.grubelnik@datadialog.net), DataDialog',
     'version': '1.0',
     'website': 'https://www.datadialog.net',
     'installable': True,
     'depends': [
-        'fsonline',
+        'sale',
+        'portal_sale',
+        'website',
+        'website_sale_donate',
     ],
-    'data': [],
+    'data': [
+        'views/templates.xml',
+        'views/snippet_options.xml',
+    ],
 }
-
-    
